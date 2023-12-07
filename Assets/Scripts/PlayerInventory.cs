735a486daa45dabc0ba2b94d96bd172f735ee568
Jake Mallaby <jakemallaby007@gmail.com>
2023-12-07 18:40:00 +0000

    shop works can now buy characters

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -9,9 +9,56 @@ public class PlayerInventory : MonoBehaviour
     private int coins = 0;
     public Text coinsText;
 
+    private const string CoinsPrefsKey = "PlayerCoins";
+
+    private static PlayerInventory _instance;
+
+    public static PlayerInventory Instance
+    {
+        get
+        {
+            // Lazy initialization of the instance
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<PlayerInventory>();
+            }
+            return _instance;
+        }
+    }
+
+    public void RemoveCoins(int amount)
+    {
+        coins -= amount;
+        SaveCoins();
+        UpdateCoinsUI();
+    }
+
     public void AddCoins(int amount)
     {
         coins += amount;
+        SaveCoins();
+        UpdateCoinsUI();
+    }
+
+    private void SaveCoins()
+    {
+        // Save the current coins value to PlayerPrefs
+        PlayerPrefs.SetInt(CoinsPrefsKey, coins);
+    }
+
+    public int GetCoins()  // Corrected method name
+    {
+        return coins;
+    }
+
+    public void LoadCoins()
+    {
+        // Load coins from PlayerPrefs
+        coins = PlayerPrefs.GetInt(CoinsPrefsKey, 0);
+    }
+
+    private void UpdateCoinsUI()
+    {
         //Debug.Log("Coins: " + coins);
         //update coins UI 
         coinsText.text = "Coins: " + coins;
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class ShopManager : MonoBehaviour
 {
 
     public int charIndex;
     public GameObject[] characterModels;
 
+    public CharacterBp[] charsBp;
+    public Button buyButton;
 
     void Start()
     {
+        foreach(CharacterBp character in charsBp){
+            if (character.price == 0)
+            {
+                character.isUnlocked = true;
+            }
+            else {
+                int isUnlockedValue = PlayerPrefs.GetInt(character.name, 0);
+                character.isUnlocked = isUnlockedValue == 1; // Assuming 1 means unlocked, 0 means locked
+            }
+
+        }
         charIndex = PlayerPrefs.GetInt("SelectedChar", 0);
         foreach (GameObject character in characterModels) {
             character.SetActive(false);
 
             characterModels[charIndex].SetActive(true);
         }
+
+        PlayerInventory.Instance.LoadCoins();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateUI();
     }
 
     public void nextChar() {
@@ -35,6 +51,11 @@ public class ShopManager : MonoBehaviour
         }
 
         characterModels[charIndex].SetActive(true);
+        CharacterBp ch = charsBp[charIndex];
+        if (!ch.isUnlocked) {
+            return;
+            
+        }
         PlayerPrefs.SetInt("SelectedChar", charIndex);
     }
 
@@ -49,6 +70,47 @@ public class ShopManager : MonoBehaviour
         }
 
         characterModels[charIndex].SetActive(true);
+        CharacterBp ch = charsBp[charIndex];
+        if (!ch.isUnlocked)
+        {
+            return;
+            
+        }
         PlayerPrefs.SetInt("SelectedChar", charIndex);
+    }
+
+    private void UpdateUI() {
+        CharacterBp ch = charsBp[charIndex];
+
+        if (ch.isUnlocked)
+        {
+            buyButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            buyButton.gameObject.SetActive(true);
+            buyButton.GetComponentInChildren<TextMeshProUGUI>().text = "Buy -" + ch.price;
+
+            // Use PlayerInventory to check coins
+            if (ch.price < PlayerInventory.Instance.GetCoins())  // Corrected method name
+            {
+                buyButton.interactable = true;
+            }
+            else
+            {
+                buyButton.interactable = false;
+            }
+        }
+    }
+
+    public void unlockCharacter() {
+          CharacterBp ch = charsBp[charIndex];
+
+    // Use PlayerInventory to deduct coins
+    PlayerInventory.Instance.RemoveCoins(ch.price);
+
+    PlayerPrefs.SetInt(ch.name, 1);
+    PlayerPrefs.SetInt("SelectedChar", charIndex);
+    ch.isUnlocked = true;
     }
 }
