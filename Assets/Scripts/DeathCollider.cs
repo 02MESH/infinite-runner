212670fa7f22094a419501034e7c3f7b319f17aa
Meshwil <meshwildias@gmail.com>
2023-12-12 13:29:25 +0000

    Car front-end box-collider with script added

--- Assets/Scripts/DeathCollider.cs
+++ Assets/Scripts/DeathCollider.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DeathCollider : MonoBehaviour
 {
-    private void OnTriggerEnter(Colider other) {
+    private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")) {
             Debug.Log("Player touched collider");
         }
