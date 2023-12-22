42f6a6225b84eb794a6405f3df44539fc57acef1
Meshwil <meshwildias@gmail.com>
2023-12-22 11:49:15 +0000

    Tiles and Truck movement
    * Tile generation partially complete
    * Vehicle movement i.e. truck complete
    * Need to add more vehicle objects as well and implement inheritance.

--- Assets/Scripts/TruckMovement.cs
+++ Assets/Scripts/TruckMovement.cs
@@ -8,11 +8,48 @@ public class TruckMovement : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "rotateVehicleRight")
-        {
-            Debug.Log("Enter");
-            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
-            other.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        GameObject collidedObject = other.gameObject;
+        if(collidedObject.transform.parent != null && (other.gameObject.name == "rotateVehicleRight") || other.gameObject.name == "rotateVehicleLeft") { 
+            Transform parentObject = collidedObject.transform.parent;
+            Transform truckAndTrailer = parentObject.Find("truckAndTrailer");
+            Debug.Log(collidedObject.transform.parent.gameObject.name);
+            Debug.Log(truckAndTrailer);
+            if(other.gameObject.name == "rotateVehicleRight") {
+                Debug.Log("Right Switch");
+                leftToRight(truckAndTrailer);
+            } else if(other.gameObject.name == "rotateVehicleLeft") {
+                Debug.Log("Left switch");
+                rightToLeft(truckAndTrailer);
+            }
         }
+    }
+
+    void leftToRight(Transform parentTransform) {
+        float newXPosition = parentTransform.position.x + 3f;
+        float currentYPosition = parentTransform.position.y;
+        float currentZPosition = parentTransform.position.z - 4f;
+
+        // Create a new Vector3 with the updated x position and the same y and z positions
+        Vector3 newPosition = new Vector3(newXPosition, currentYPosition, currentZPosition);
+
+        // Assign the new position to the Transform's position property
+        parentTransform.position = newPosition;
+        
+        // parentTransform.position = new Vector3(17f,0f,18f);
+        parentTransform.rotation = Quaternion.Euler(0f, -90f, 0f);
+    }
+
+    void rightToLeft(Transform parentTransform) {
+        float newXPosition = parentTransform.position.x + 3f;
+        float currentYPosition = parentTransform.position.y;
+        float currentZPosition = parentTransform.position.z + 4f;
+
+        // Create a new Vector3 with the updated x position and the same y and z positions
+        Vector3 newPosition = new Vector3(newXPosition, currentYPosition, currentZPosition);
+
+        // Assign the new position to the Transform's position property
+        parentTransform.position = newPosition;
+
+        parentTransform.rotation = Quaternion.Euler(0f, 90f, 0f);
     }
 }
