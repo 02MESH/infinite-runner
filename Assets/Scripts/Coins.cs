6a4b5b674c3d4fb3de4323f4724029021eeb0bba
Meshwil <meshwildias@gmail.com>
2023-12-22 17:20:46 +0000

    Magnet power-up
    *Need to add the time for the power-up
    *Need to fix the distance for the activation coz all the coins get pulled

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Coins : Collectible
 {
-
+    public Transform playerTransform;
+    public float moveSpeed = 10f;
     // Number of coins to add when collected
     public int coinsToAdd = 1;
     public AudioClip coinCollectSound;
+    CoinMove coinMoveScript;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("characterSelect").GetComponent<CharacterSelect>().getTransform();
+        coinMoveScript = gameObject.GetComponent<CoinMove>();
+    }
+
     public override void ApplyEffect(Transform playerTransform)
     {
         // Add coins to the player's inventory or score
